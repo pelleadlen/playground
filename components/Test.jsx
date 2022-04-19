46a2d8409f755f33f63d21efa1cd647ee30fb960
pelleadlen <92332213+pelleadlen@users.noreply.github.com>
2022-04-19 23:00:19 +0200

    commit
    commit

---
+++ components/Test.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+const Test = () => {
+  return (
+    <div>
+      <Image src="/test.png" width={400} height={400} />
+    </div>
+  );
+};
+export default Test;
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Test from "../components/Test";
+
+const index = () => {
+  return (
+    <div className="flex justify-center">
+      <Test />
+      <h1 className="text-3xl">Hey</h1>
+    </div>
+  );
+};
+export default index;
