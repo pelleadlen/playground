2543f057f0c2defe39a733476dfe83954786035e
pelleadlen <92332213+pelleadlen@users.noreply.github.com>
2022-06-15 12:47:56 +0200

    change name

--- components/hooks/fadeIn.js
+++ components/hooks/fadeIn.js
@@ -6,20 +6,24 @@ export const FadeIn = ({ children, className }) => {
   const [ref, inView] = useInView({
     threshold: 0.2,
   });
+
   const animation = useAnimation();
 
   useEffect(() => {
     console.log(inView);
     if (inView) {
-      animation.start({
-        y: 0,
-        opacity: 1,
-        transition: {
-          duration: 0.9,
-          type: "spring",
-          bounce: 0.1,
+      animation.start(
+        {
+          y: 0,
+          opacity: 1,
+          transition: {
+            duration: 0.9,
+            type: "spring",
+            bounce: 0.1,
+          },
         },
-      });
+        [animation]
+      );
     }
     if (!inView) {
       animation.start({
