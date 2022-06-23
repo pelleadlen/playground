32f8ed0a42a66ffdcfa08ee9e99b1b37a7d8f0c3
pelleadlen <92332213+pelleadlen@users.noreply.github.com>
2022-06-24 00:12:38 +0200

    Fixed hover

--- pages/_document.js
+++ pages/_document.js
@@ -33,8 +33,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#f2f2f2" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
