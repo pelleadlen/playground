ee66659f8e8743e45f5c21c2969130d297fd9f57
pelleadlen <92332213+pelleadlen@users.noreply.github.com>
2022-07-04 12:14:01 +0200

    text styling

--- styles/styledOurlegacy.js
+++ styles/styledOurlegacy.js
@@ -14,12 +14,14 @@ export const Text = styled.h2`
   font-size: clamp(1.5rem, 4vw, 2rem);
   font-weight: 300;
   line-height: 1.4;
-  width: 60%;
+  width: 50%;
   letter-spacing: -0.02em;
   color: ${(props) => (props.title ? "black" : "gray")};
   padding-bottom: ${(props) => (props.title ? "0.5rem" : "0")};
   align-self: flex-start;
-
+  @media (max-width: 1024px) {
+    width: 80%;
+  }
   @media (max-width: 768px) {
     width: 100%;
   }
