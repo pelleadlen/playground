6951358fe9eeb002b4ddb53fc751082300d897d1
pelleadlen <92332213+pelleadlen@users.noreply.github.com>
2022-08-27 18:40:24 +0200

    added lazyloading for heavy imgs

--- components/casestudies/ourlegacy/imageGrid.jsx
+++ components/casestudies/ourlegacy/imageGrid.jsx
@@ -9,60 +9,63 @@ import {
   MobileSection,
 } from "./styled/styledImageGrid";
 import Image from "next/image";
+import { RevealUp } from "../../hooks/animation";
 
 export const ImageGrid = () => {
   return (
     <>
-      <Section>
-        <Grid>
-          <ImgWrapper>
-            <Img
-              alt="loading screen of our legacy"
-              width="100%"
-              height="100%"
-              layout="fill"
-              src="/Assets/Images/siteloader.png"
-            />
-          </ImgWrapper>
+      <RevealUp cascade={true}>
+        <Section>
+          <Grid>
+            <ImgWrapper>
+              <Img
+                alt="loading screen of our legacy"
+                width="100%"
+                height="100%"
+                layout="fill"
+                src="/Assets/Images/siteloader.png"
+              />
+            </ImgWrapper>
 
-          <ImgWrapper>
-            <Img
-              alt="signup modal as card"
-              width="100%"
-              height="100%"
-              layout="fill"
-              src="/Assets/Images/signup.png"
-            />
-          </ImgWrapper>
-          <ImgWrapper>
-            <Img
-              alt="store section"
-              width="100%"
-              height="100%"
-              layout="fill"
-              src="/Assets/Images/store.png"
-            />
-          </ImgWrapper>
-          <ImgWrapper>
-            <Img
-              alt="selected item"
-              width="100%"
-              height="100%"
-              layout="fill"
-              src="/Assets/Images/item.png"
-            />
-          </ImgWrapper>
-          <ImgWrapper>
-            <Img
-              alt="blue offwhite and black colors"
-              width="100%"
-              height="100%"
-              layout="fill"
-              src="/Assets/Images/colors.png"
-            />
-          </ImgWrapper>
-        </Grid>
-      </Section>
+            <ImgWrapper>
+              <Img
+                alt="signup modal as card"
+                width="100%"
+                height="100%"
+                layout="fill"
+                src="/Assets/Images/signup.png"
+              />
+            </ImgWrapper>
+            <ImgWrapper>
+              <Img
+                alt="store section"
+                width="100%"
+                height="100%"
+                layout="fill"
+                src="/Assets/Images/store.png"
+              />
+            </ImgWrapper>
+            <ImgWrapper>
+              <Img
+                alt="selected item"
+                width="100%"
+                height="100%"
+                layout="fill"
+                src="/Assets/Images/item.png"
+              />
+            </ImgWrapper>
+            <ImgWrapper>
+              <Img
+                alt="blue offwhite and black colors"
+                width="100%"
+                height="100%"
+                layout="fill"
+                src="/Assets/Images/colors.png"
+              />
+            </ImgWrapper>
+          </Grid>
+        </Section>
+      </RevealUp>
     </>
   );
 };
--- components/casestudies/ourlegacy/mobileImageGrid.jsx
+++ components/casestudies/ourlegacy/mobileImageGrid.jsx
@@ -1,52 +1,55 @@
+import { RevealUp } from "../../hooks/animation";
 import { ImgSm, ImgWrapperSm, Section } from "./styled/styledMobileImageGrid";
 
 export const MobileImageGrid = () => {
   return (
     <>
-      <Section>
-        <ImgWrapperSm>
-          <ImgSm
-            alt="loading screen of our legacy"
-            width="100%"
-            height="100%"
-            layout="fill"
-            src="/Assets/Images/siteloader-mobile.png"
-          />
-        </ImgWrapperSm>
-        <ImgWrapperSm>
-          <ImgSm
-            alt="signup modal as card"
-            width="100%"
-            height="100%"
-            layout="fill"
-            src="/Assets/Images/signup-mobile.png"
-          />
-        </ImgWrapperSm>
-        <ImgWrapperSm>
-          <ImgSm
-            width="100%"
-            height="100%"
-            layout="fill"
-            src="/Assets/Images/store-mobile.png"
-          />
-        </ImgWrapperSm>
-        <ImgWrapperSm>
-          <ImgSm
-            width="100%"
-            height="100%"
-            layout="fill"
-            src="/Assets/Images/item-mobile.png"
-          />
-        </ImgWrapperSm>
-        <ImgWrapperSm>
-          <ImgSm
-            width="100%"
-            height="100%"
-            layout="fill"
-            src="/Assets/Images/colors-mobile.png"
-          />
-        </ImgWrapperSm>
-      </Section>
+      <RevealUp>
+        <Section>
+          <ImgWrapperSm>
+            <ImgSm
+              alt="loading screen of our legacy"
+              width="100%"
+              height="100%"
+              layout="fill"
+              src="/Assets/Images/siteloader-mobile.png"
+            />
+          </ImgWrapperSm>
+          <ImgWrapperSm>
+            <ImgSm
+              alt="signup modal as card"
+              width="100%"
+              height="100%"
+              layout="fill"
+              src="/Assets/Images/signup-mobile.png"
+            />
+          </ImgWrapperSm>
+          <ImgWrapperSm>
+            <ImgSm
+              width="100%"
+              height="100%"
+              layout="fill"
+              src="/Assets/Images/store-mobile.png"
+            />
+          </ImgWrapperSm>
+          <ImgWrapperSm>
+            <ImgSm
+              width="100%"
+              height="100%"
+              layout="fill"
+              src="/Assets/Images/item-mobile.png"
+            />
+          </ImgWrapperSm>
+          <ImgWrapperSm>
+            <ImgSm
+              width="100%"
+              height="100%"
+              layout="fill"
+              src="/Assets/Images/colors-mobile.png"
+            />
+          </ImgWrapperSm>
+        </Section>
+      </RevealUp>
     </>
   );
 };
